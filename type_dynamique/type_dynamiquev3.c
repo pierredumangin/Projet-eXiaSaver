d93cdf75c08b53b778cf69f94c620ef5628740cd
pierredumangin <pierredumangin@users.noreply.github.com>
2016-12-15 13:49:02 +0100

    Add files via upload

--- type_dynamique/type_dynamiquev3.c
+++ type_dynamique/type_dynamiquev3.c
@@ -17,44 +17,46 @@ int unitesecondes = 0;
 char tableau[11][30];  // tableau qui contient tout les tableaux contenant les caractères des chiffres
 
 int j = 0;
-char caractere;
+char caractere;  // char qui va parcourir les fichiers PBM
 
 
 int main(){
 
 
-  const char *PBM2 = "EXIASAVER2_PBM";
-  char *PBm2;
-  PBm2 = getenv(PBM2);
+  const char *PBM2 = "EXIASAVER2_PBM"; // création du tableau de char contenant le nom de la variable d'envirronement
+  char *PBm2;   // création du tableau de char qui va contenir le nom de la variable d'envirronement
+  PBm2 = getenv(PBM2);  //affectation à PBm2 du contenu de PBM2
 
 
 
-int u =1;
-system("clear");
-remplirtableau(PBm2);
+int u =1; // permet de faire uen boucle infinie
+system("clear");  // nettoie la console
+remplirtableau(PBm2); // remplit les lignes des tableaux avec le contenu des PBM
 
-while(u == 1){
+while(u == 1){  // boucle infinie
 
-  int i = 0;
 
-  time_t secondes;
 
-  time(&secondes);
+  time_t secondes;  // création de la variable secondes, de type time_t
 
-  instant=*localtime(&secondes);
-  int aa;
-  int a = 9;
+  time(&secondes);   // affectation à secondes du nombres de secondes écoulées depuis le 1er janvier 1970
+
+  instant=*localtime(&secondes);  // remplissage de la structure tm instant avec les données contenues dans secondes et converties grâce à localtime()
+
+
+  int aa;       // compteur permettant de parcourir les tableaux
+  int a = 9;    // position initiale du curseur quand on commence à afficher les chiffres
   int b = 14;
-  int x = 18;
+  int x = 18;   // position initiale du curseur quand on affiche que l'écran va s'actualiser
   int y = 15;
 
-  decouperheures(instant);
+  decouperheures(instant);  // affecte aux valeurs précédemment citées les valeurs des dizaines d'heures etc
 
 
 ///////////////////////////////////////PEMIERE LIGNE
 
 ////////////////////////////////// DIZAINES HEURES
-gotoxy(a,b);
+gotoxy(a,b);     // positionne le curseur dans le terminal
 
         for (aa = 0; aa < 6 ; aa++) // on parcours les premiers caractères de l'image correspondant au
         // chiffre des dizaine d'heures
@@ -442,20 +444,24 @@ for (aa = 24; aa < 30 ; aa++)
   }
 
 printf(" ");
-int e;
-int refreshTime = 1;
-gotoxy(x,y);
+
+
+///////////////////////////////////////////////////////// FIN DE L IMPRESSION DE L HEURE
+
+int e;   // compteur
+int refreshTime = 10;   // nombre de secondes entre chaque rafraichissement
+gotoxy(x,y);            //  on place le curseur à cette position
 printf("Cet écran va s'actualiser dans quelques secondes" );
 
-for (e = 0; e < refreshTime; e++)
+for (e = 0; e < refreshTime; e++)  // on imprime un point toutes les secondes
 {
 sleep(1);
  gotoxy(x,y+48);
 
  printf(".\n");
- y++;
+ y++;  // on incrémente d'un les abscisses afin de placer un point à la droite du précédent
 }
-y=y-refreshTime;
+y=y-refreshTime;  // on replace le curseur à la fin de "cet écran..."
 gotoxy(x, y+47);
 printf("                                           ");
 
@@ -469,7 +475,7 @@ return 0;
 
 }
 
-int decouperheures(struct tm instant)
+int decouperheures(struct tm instant) // remplit les différentes variables utilisées pour afficher l'heure
 {
 
 dizaineheure = (instant.tm_hour)/10 ;
@@ -484,7 +490,7 @@ unitesecondes = (instant.tm_sec)-(dizainesecondes*10);
 remplirtableau(char* PBm2)
 {
 
-
+  // complète chaque char avec la variable d'envirronement + le chemin vers les différentes images
   char* Pbm0 = concat(PBm2, "/0.pbm");
   char* Pbm1 = concat(PBm2, "/1.pbm");
   char* Pbm2 = concat(PBm2, "/2.pbm");
@@ -496,64 +502,68 @@ remplirtableau(char* PBm2)
   char* Pbm8 = concat(PBm2, "/8.pbm");
   char* Pbm9 = concat(PBm2, "/9.pbm");
   char* Pbmpoint = concat(PBm2, "/10.pbm");
+
+// complète le tableau à deux dimensions afin que les lignes contiennent les caracteres qui composent les chiffres
 ///////////////////////////////////////////////////   NUMERO 0
-FILE* fichier0 = NULL;
+FILE* fichier0 = NULL;  // initialise le pointeur fichier0 à NULL
 
-fichier0 = fopen(Pbm0, "r") ;
+fichier0 = fopen(Pbm0, "r") ; // ouvre le fichier
 
-int g =0;
-if (fichier0 != NULL){
+int g; //  variable qui permet de se déplacer entres les différentes lignes
+if (fichier0 != NULL){        // on vérifie que le fichier a été ouvert
 
 
-  fseek(fichier0, 18, SEEK_SET);
-//  gotoxy(a,b);
- j = 0;
- g = 0;
+  fseek(fichier0, 18, SEEK_SET); // on se place au 18eme caractere afin de ne pas lire les dimensions de l'image et commencer directement à la série de 0 et de 1
+
+ j = 0;   // on initialise la colonne à 0
+ g = 0;  // on initialise la ligne à 0 pour remplir la première ligne du tableau, ici avec le chiffre 0
   do{
 
-    caractere = fgetc(fichier0);
-    if ( caractere == '0')
+    caractere = fgetc(fichier0); // on replit le char caractere avec le caractere lu dans le fichier
+    if ( caractere == '0')  // si le caractère est un 0,
     {
 
-    tableau[g][j] = ' ';
-    j++;
+    tableau[g][j] = ' ';  // on place un espace dans le tableau
+    j++; // on passe à la colonne suivante
     }
-   else if ( caractere == ' ')
+   else if ( caractere == ' ')  // si le caractère est un espace
     {
 
-      tableau[g][j] = ' ';
+      tableau[g][j] = ' '; // on place un espace dans le tableau
       j++;
     }
-    else if ( caractere == '1')
+    else if ( caractere == '1')  // si le caractère est un 1
     {
 
-      tableau[g][j] = 'X';
+      tableau[g][j] = 'X';  // on place un X dans le tableau
       j++;
     }
-   else if ( caractere == '\n')
+   else if ( caractere == '\n')  // si on a un retour à la ligne,
     {
 
-      tableau[g][j] = ' ';
+      tableau[g][j] = ' '; // on place un espace dans le tableau
       j++;
 
     }
-  }while(caractere != EOF);
+  }while(caractere != EOF);   // on parcourt le fichier jusqu'à sa fin
 }
   else {
 
-   printf("Le fichier ne peut pas être ouvert \n");
+   printf("Le fichier ne peut pas être ouvert \n"); // si le fichier n'a pas pu être ouvert, on affiche le message d'erreur
 
   }
-  fclose(fichier0);
+  fclose(fichier0);  // on ferme le fichier ouvert précédemment
 
 //////////////////////////////////////////   NUMERO 1
-  g++;
+  g++; // on change de ligne
+
+  // on répète l'opération précédemment faite pour chaque chiffre
   FILE* fichier1 = NULL;
   fichier1 = fopen(Pbm1, "r") ;
   if (fichier1 != NULL){
 
     fseek(fichier1, 18, SEEK_SET);
-  //  gotoxy(a,b);
+
    j = 0;
     do{
 
@@ -689,7 +699,7 @@ fichier4 = fopen(Pbm4, "r") ;
 if (fichier4 != NULL){
 
   fseek(fichier4, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
@@ -735,7 +745,7 @@ fichier5 = fopen(Pbm5, "r") ;
 if (fichier5 != NULL){
 
   fseek(fichier5, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
@@ -780,7 +790,7 @@ fichier6 = fopen(Pbm6, "r") ;
 if (fichier6 != NULL){
 
   fseek(fichier6, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
@@ -821,7 +831,7 @@ fichier7 = fopen(Pbm7, "r") ;
 if (fichier7 != NULL){
 
   fseek(fichier7, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
@@ -866,7 +876,7 @@ fichier8 = fopen(Pbm8, "r") ;
 if (fichier8 != NULL){
 
   fseek(fichier8, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
@@ -912,7 +922,7 @@ fichier9 = fopen(Pbm9, "r") ;
 if (fichier9 != NULL){
 
   fseek(fichier9, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
@@ -958,7 +968,7 @@ fichier10 = fopen(Pbmpoint, "r") ;
 if (fichier10 != NULL){
 
   fseek(fichier10, 18, SEEK_SET);
-//  gotoxy(a,b);
+
  j = 0;
   do{
 
